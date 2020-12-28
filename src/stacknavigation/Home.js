f26ebbd60ecafe8f6cd3c1c2bc429efcfbbff4e8
phamtanphat <phatdroid94@gmail.com>
2020-12-28 18:25:19 +0700

    pass callback function

--- src/stacknavigation/Home.js
+++ src/stacknavigation/Home.js
@@ -1,5 +1,6 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
 
 export default class Home extends Component {
   constructor(props) {
@@ -8,13 +9,20 @@ export default class Home extends Component {
   }
   render() {
     return (
-      <View style={{flex: 1}}>
-        <Text> textInComponent </Text>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <TouchableOpacity
+          onPress={() => {
+            this.props.route.params.onChangeText('Update finish');
+            this.props.navigation.navigate('Login');
+          }}
+          style={{backgroundColor: 'green', padding: 10, borderRadius: 5}}>
+          <Text style={{color: 'white'}}>{this.props.route.params.user}</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 
   componentWillUnmount() {
-    console.log('un mount screen');
+    console.log('un mount screen Home');
   }
 }
--- src/stacknavigation/Login.js
+++ src/stacknavigation/Login.js
@@ -4,15 +4,32 @@ import {Text, View} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Navigate Screen Home',
+    };
+  }
+  onChangeText = (text) => {
+    this.setState({text});
+  };
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('Home')}
+          onPress={() =>
+            this.props.navigation.navigate('Home', {
+              user: 'reactnavigation',
+              onChangeText: this.onChangeText,
+            })
+          }
           style={{backgroundColor: 'green', padding: 10, borderRadius: 5}}>
-          <Text style={{color: 'white'}}>Navigate Screen Home</Text>
+          <Text style={{color: 'white'}}>{this.state.text}</Text>
         </TouchableOpacity>
       </View>
     );
+  }
+  componentWillUnmount() {
+    console.log('un mount screen Login');
   }
 }
